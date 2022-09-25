e0cca52f62386d640359c59e01f0048b7a9468d6
Anon_x_31 <99769246+Anon3172@users.noreply.github.com>
2022-09-25 16:52:45 +0530

    first repository

---
+++ script.js
@@ -0,0 +1,31 @@
+const showBtn = document.querySelector('.navBtn');
+const topNav = document.querySelector('.top-nav');
+
+showBtn.addEventListener('click', function(){
+    if(topNav.classList.contains('showNav')){
+        topNav.classList.remove('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-bars"></i>';
+    } else {
+        topNav.classList.add('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-times"></i>';
+    }
+});
+
+/* Lightbox */
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+/* Scroll Top Button*/
+ let backtotop = select('.back-to-top')
+ if (backtotop) {
+   const toggleBacktotop = () => {
+     if (window.scrollY > 100) {
+       backtotop.classList.add('active')
+     } else {
+       backtotop.classList.remove('active')
+     }
+   }
+   window.addEventListener('load', toggleBacktotop)
+   onscroll(document, toggleBacktotop)
+ }
+
+ 
